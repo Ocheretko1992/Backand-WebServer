8bb84110379e384c7188501949ab5a0308aeac78
Ocheretko Oleh <117608037+Ocheretko1992@users.noreply.github.com>
2025-04-11 16:14:07 +0300

    fix

--- src/db/initMongoConection.js
+++ src/db/initMongoConection.js
@@ -1,13 +1,12 @@
-import mongoose from "mongoose";
-import { getEnvVar } from "../utils/getEnvVar.js";
-
+import mongoose from 'mongoose';
+import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoConection = async () => {
   try {
     const password = getEnvVar('MONGODB_PASSWORD');
-    
+
     await mongoose.connect(
-      `mongodb+srv://Ocheretko:${password}@cluster0.qgp7txo.mongodb.net/my-movies?retryWrites=true&w=majority&appName=Cluster0`,
+      `mongodb+srv://Oleh:${password}@cluster0.qgp7txo.mongodb.net/my-movies?retryWrites=true&w=majority&appName=Cluster0`,
     );
   } catch (error) {
     console.log(error.message);
